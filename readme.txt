8de582c4eeca5a0a0a9f44345f775c03ad15f3ee
Silver <Silver@192.168.1.101>
2016-05-16 10:34:48 +0300

    First full version

---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== WS Contact Form ===
+Contributors: silvermuru
+Tags:  contact, contact form, contact form plugin, contact forms, contact us, email, feedback form, feedback forms, form, forms
+Requires at least: 4.4
+Tested up to: 4.6
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Wordpress plugin for simple contact form what will allow to send message to desired email
+
+== Description ==
+
+Simple contact form what will allow to send message to desired email. Simple to use webpage own styles, the default form is almoust without its own styles, what need to overwrited. Working with shortcode through ajax
+
+== Installation ==
+
+1. Upload the plugin files to the '/wp-content/plugins/plugin-name' directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress
+3. Fill settings fiels on Wordpress Admin Settings > WS Contact Form page
+4. Paste plugin shortcode to your post/page in Text view activated
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+
+
+
+== Changelog ==
+
+= 1.0 =
+
+First version
+
+
+== Upgrade Notice ==
--- ws-contact-form.php
+++ ws-contact-form.php
@@ -16,17 +16,79 @@ if ( !defined( 'ABSPATH' ) ) {
 class WS_Contact_Form {	
 	public function __construct(){
 		add_action( 'plugins_loaded', array( $this, 'ws_contact_form_load_textdomain' ) );
-        add_action( 'plugins_loaded', array( $this, 'check_if_user_logged_in' ) );
-        add_action( 'init', array( $this, 'ws_force_login_message_show' ) );
+        add_action( 'wp_footer', array( $this, 'ws_contact_form_sript' ) );
+        add_action( 'wp_enqueue_scripts', array( $this, 'ws_contact_form_style' ) );
+        add_action( 'wp_ajax_contacthomepage', array( $this, 'ws_contact_form_sendmail' ) );
+        add_action( 'wp_ajax_nopriv_contacthomepage', array( $this, 'ws_contact_form_sendmail' ) );
+        add_shortcode( 'ws-contact-form', array( $this, 'ws_contact_form_shortcode' ) );
     }
     
     public function ws_contact_form_load_textdomain() {
         load_plugin_textdomain( 'ws-contact-form', false, plugin_basename( dirname( __FILE__ ) ) . '/languages/' ); 
-	}    
+	}
+    
+    public function ws_contact_form_shortcode( $atts, $content = null ){
+        if (get_option('wscontact-form-title-option') != false) {
+            echo '<h3> ' . get_option('wscontact-form-title-option') . '</h3>';
+        }
+        ?>
+        <form id="ws_contact_form" class="ws-form-group">
+            <div class="ws-hidden ws-message-response ws-message-successful" id="ws_contact_success"><?php echo _e( 'Message is sent successfully!', 'ws-contact-form' ); ?></div>
+            <div class="ws-hidden ws-message-response ws-message-error" id="ws_contact_error"><?php echo _e( 'There is problem with form', 'ws-contact-form' ); ?></div>
+            <input class="ws-form-control" type="text" name="ws_contact_name" id="ws_contact_name" placeholder="<?php echo _e( 'Name', 'ws-contact-form' ); ?> *">
+            <input class="ws-form-control" type="text" name="ws_contact_email" id="ws_contact_email" placeholder="<?php echo _e( 'Email', 'ws-contact-form' ); ?> *">
+            <textarea class="ws-form-control" name="ws_contact_comment" id="ws_contact_comment" placeholder="<?php echo _e( 'Content', 'ws-contact-form' ); ?> *"></textarea>
+            <button type="submit" id="ws_send_form"><?php echo _e( 'Send', 'ws-contact-form' ); ?></button>
+        </form>
+        <?php
+    }
+    
+    public function ws_contact_form_sript() {
+        wp_register_script( 'ws-contact-form-script', plugin_dir_url(__FILE__) . 'js/scripts.js', array( 'jquery' ), false );
+        wp_enqueue_script( 'ws-contact-form-script' );
+    }
+    
+    public function ws_contact_form_style() {
+		wp_register_style( 'ws-contact-form-style', plugin_dir_url(__FILE__) . 'css/style.css', true );
+        wp_enqueue_style( 'ws-contact-form-style' );
+	}
+
+    public function ws_contact_form_sendmail()
+    {
+        $email = $_POST['ws_contact_email'];
+        $headers[] = 'Content-Type: text/html; charset=UTF-8';
+        $headers[] = "From: " . get_option('wscontact-form-sender-name-option') . " <" . get_option('wscontact-form-sender-email-option') . "> \r\n".'Reply-To: '.$email;
+        $message = $_POST['ws_contact_comment'];
+        $name = $_POST['ws_contact_name'];
+        
+        function ws_email_valid($email) {
+            $email_pattern = "/^[a-z\d](\.?[a-z\d_\-]+)*@([a-z\d](\.?[a-z\d\-]+)*\.[a-z]{2,4})$/i";
+            $result = preg_match( $email_pattern, $email, $match );
+            if ( FALSE !== $result && $result > 0 )
+            {
+                return TRUE;
+            }
+            return FALSE;
+        }
+
+        if (isset($name) && ws_email_valid($email) && isset($message))
+        {
+            wp_mail( get_option('wscontact-form-recipient-name-option') . " <" . get_option('wscontact-form-recipient-email-option') . ">", get_option('wscontact-form-email-title-option'), get_option('wscontact-form-email-title-option') . ":\r\n " . __( 'Name', 'ws-contact-form' ) . ": " . $name . " \r\n " . __( 'Email', 'ws-contact-form' ) . ": " . $email . " \r\n " . __( 'Content', 'ws-contact-form' ) . ": ". $message, $headers);
+            echo 'OK';
+        }
+        else {
+            echo 'ERROR';
+        }
+
+        die();
+    }
+    
+    
 }
 
 if ( is_admin() ) {
     require plugin_dir_path( __FILE__ ) . '/admin/ws-contact-form-admin.php';
+    $wpse_ws_contact_form_plugin_admin = new WS_Contact_Form_admin();
 }
     
 $wpse_ws_contact_form_plugin = new WS_Contact_Form();
